4c023fe1a6353699030909f40f7300085fd8c770
100X_Dev <admin@Admins-MacBook-Pro.local>
2025-03-10 16:48:18 +0530

    API

---
+++ model/Order.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  totalAmount: { type: Number, required: true },
+  status: { type: String, default: "Pending" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Order = mongoose.model("Order", OrderSchema);
+
+export default Order;
