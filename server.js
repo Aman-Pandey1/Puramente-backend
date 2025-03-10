5aea8a3e95a8e41bd15564c2a09a157d15aa5b2b
100X_Dev <admin@Admins-MacBook-Pro.local>
2025-03-10 22:26:19 +0530

    API complete

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
 import connectDB from "./config/db.js";
 import userRegister from "./routes/userRoute.js";
 import productRoutes from "./routes/productRoutes.js"
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderroutes.js"
+import wishListRoutes from "./routes/wishListRoutes.js"
 
 dotenv.config();
 connectDB();
@@ -21,11 +23,14 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use(bodyParser.json());
+
 // Routes
 app.use("/api/users", userRegister);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/wishlist", wishListRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
